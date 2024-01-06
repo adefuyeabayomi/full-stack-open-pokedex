0a5d671ec67a0216a68fae78f15acad09bfaa6ca
adefuyeabayomi <63407534+adefuyeabayomi@users.noreply.github.com>
2024-01-06 09:49:06 +0100

    updated app.js with ping url, added ping with

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+app.get('/ping/',(req,res) => {
+  req.rawHeaders
+  res.writeHead(200,'OK')
+  res.end('Ping - Pong')
+})
+
 app.listen(PORT,'0.0.0.0' , () => {
 })
 
